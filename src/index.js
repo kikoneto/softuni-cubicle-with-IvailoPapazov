93f32171f58c72afef1d47b40c8ac48241f56303
KrischunIT <kikoner81@gmail.com>
2022-08-15 01:05:25 +0300

    render home page
    Moved public to root folder, Added handlebars and fixed resources

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+
 const port = 5000;
 
 const app = express();
 
-app.use('/static', express.static('public'));
+app.use('/static', express.static('../public'));
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index')
 })
 
-app.listen(port, () => console.log(`App is listening on port ${port}`));
+app.listen(port, () => console.log(`App is listening on port ${port}...`));
